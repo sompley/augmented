4414628f8dbc948809814ae2c23b1aef3113c17c
SBC4 <SBC4@SBC09>
2015-04-17 17:18:13 +0700

    hahahaha

---
+++ src/bintang/Bintang.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bintang;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Bintang {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
